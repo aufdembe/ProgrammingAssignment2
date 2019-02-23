d2b0c72a0af32fb581c152629458f4a3d3f4600c
MacMark <aufdembe@upenn.edu>
2019-02-23 18:18:57 -0500

    I've written the code largely based on the cahcemean example in the assignment.  The Demystifying makeVector() article by lgreski was very helpful!  I've rewritten my comments in a way that I feel explains whats going on better than the cachemean comment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following two functions will cache a "square' matrix and its inverse.
 
-## Write a short comment describing this function
+## This function creates list of functions that let you "set" a matrix that is  
+## cached in a different environment.  When you call cacheSolve the inverse of
+## the "set" matrix will be cached in that same environment.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      library(MASS)
+      m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      get <-function() x
+      setinv <- function(ginv) m <<- ginv
+      getinv <- function() m
+      list(set = set,
+            get = get,
+            setinv = setinv,
+            getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## This function computes the inverse of the "set" matrix returned by 
+## makeCacheMatrix above. If the inverse has already been calculated (and the 
+## matrix has not changed by calling makeCacheMatrix again), then cacheSolve 
+## should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      m <- x$getinv()
+      if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+      }
+      data <- x$get()
+      m <- ginv(data)
+      x$setinv(m)
+      m
 }
